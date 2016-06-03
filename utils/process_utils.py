4b84f6828d263ed9d07f5816ae218c0567aecc59
bugy <buggygm@gmail.com>
2016-06-03 22:25:37 +0200

    #10 added error printing when subprocess fails

--- utils/process_utils.py
+++ utils/process_utils.py
@@ -11,18 +11,22 @@ def invoke(command, work_dir="."):
                          stderr=subprocess.PIPE,
                          cwd=work_dir)
 
-    (output, error) = p.communicate()
+    (output_bytes, error_bytes) = p.communicate()
 
-    output = output.decode("utf-8")
+    output = output_bytes.decode("utf-8")
+    error = error_bytes.decode("utf-8")
 
     result_code = p.returncode
     if result_code != 0:
         message = "Execution failed with exit code " + str(result_code)
         six.print_(message)
         six.print_(output)
+
+        if error:
+            six.print_(" --- ERRORS ---:")
+            six.print_(error)
         raise Exception(message)
         
-    error = error.decode("utf-8")
     if error:
         six.print_("WARN! Error output wasn't empty, although the command finished with code 0!")
 
