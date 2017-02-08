6e5c84498f0af2f9109929675229424684479e32
bugy <buggygm@gmail.com>
2017-02-08 09:32:29 +0100

    fixed encode error when trying to print utf-8 process output in ascii stdout

--- utils/process_utils.py
+++ utils/process_utils.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 import os
 import subprocess
 
+import utils.string_utils as string_utils
+
 
 def invoke(command, work_dir="."):
     if isinstance(command, str):
@@ -17,20 +19,20 @@ def invoke(command, work_dir="."):
 
     (output_bytes, error_bytes) = p.communicate()
 
-    output = output_bytes.decode("utf-8")
-    error = error_bytes.decode("utf-8")
+    output = output_bytes.decode('utf-8')
+    error = error_bytes.decode('utf-8')
 
     result_code = p.returncode
     if result_code != 0:
-        message = "Execution failed with exit code " + str(result_code)
+        message = 'Execution failed with exit code ' + str(result_code)
         print(message)
-        print(output)
+        print(string_utils.utf_to_stdout(output))
 
         if error:
             print(" --- ERRORS ---:")
-            print(error)
+            print(string_utils.utf_to_stdout(error))
         raise Exception(message)
-        
+
     if error:
         print("WARN! Error output wasn't empty, although the command finished with code 0!")
 
--- utils/string_utils.py
+++ utils/string_utils.py
@@ -1,5 +1,7 @@
 import re
 
+import sys
+
 
 def contains_whole_word(text, word):
     pattern = re.compile(r'\b({0})\b'.format(word), flags=re.IGNORECASE)
@@ -29,3 +31,10 @@ def trim_text(text):
     trimmed_text = "\n".join(trimmed_lines)
 
     return remove_empty_lines(trimmed_text)
+
+
+def utf_to_stdout(utf_string):
+    if sys.stdout.encoding != 'utf-8':
+        return utf_string.encode('utf').decode(sys.stdout.encoding, 'replace')
+
+    return utf_string
